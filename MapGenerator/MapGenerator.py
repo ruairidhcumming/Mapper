3a554201019ed6297d21b5fbfa0a21e96d3f23a2
Ruairidh Cumming <ruairid.cumming@gmail.com>
2019-03-14 22:24:16 +0100

    update batching
    increase to huge batch size (2) due to hardware constraints. performance looks promising see images/maps/

--- MapGenerator/MapGenerator.py
+++ MapGenerator/MapGenerator.py
@@ -13,7 +13,7 @@ print('creating Pix2Pix model')
 model = pix2pix.Pix2Pix()
 ## that was easy, pix2pix and dataloader modified from Keras-GAN and keras-contrib respectively
 
-model.train(epochs = 4 , batch_size = 4, datapath = picspath)
+model.train(epochs = 4 , batch_size = 2, datapath = picspath)
 
 
 ##define model optimiser 
