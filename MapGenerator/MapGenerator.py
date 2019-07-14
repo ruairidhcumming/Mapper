2d4c550bd36dce71ba7f4516a4de492bb150c13f
Ruairidh Cumming <ruairid.cumming@gmail.com>
2019-07-14 12:28:19 +0100

    move towards distinch tripple input mode

--- MapGenerator/MapGenerator.py
+++ MapGenerator/MapGenerator.py
@@ -1,17 +1,19 @@
 
 import os
 import numpy as np
-import pix2pix
+from keras.utils import plot_model
+import pix3pix
 from  data_loader import DataLoader
 ### picture directories as str
 picspath = str('C:\\Users\\ruair\\Documents\\rolerball\\screenshotOverhead\\')
 
 
-print('creating Pix2Pix model')
+print('creating Pix3Pix model')
 
 #create gan model 
-model = pix2pix.Pix2Pix()
+model = pix3pix.Pix2Pix()
+## plot_model(model, to_file = 'C:\\Users\\ruair\\Documents\\rolerball')
 ## that was easy, pix2pix and dataloader modified from Keras-GAN and keras-contrib respectively
 
-model.train(epochs = 16 , batch_size = 2, datapath = picspath)
+model.train(epochs = 1 , batch_size = 2, datapath = picspath)
 
--- MapGenerator/data_loader.py
+++ MapGenerator/data_loader.py
@@ -21,26 +21,41 @@ class DataLoader():
         imgs_B = []
         for img_path in batch_images:
             img = self.imread(img_path)
-
+            #plt.imshow(img)
+            #plt.show()
             h, w, _ = img.shape
             _w = int(w/2)
-            img_A, img_B = img[:, _w:, :], img[:,:_w, :]
+            img_A, img_B ,img_C= img[:, _w:, :], img[:,:_w, :], img[:,:_w, :]
 
             img_A = scipy.misc.imresize(img_A, self.img_res)
             img_B = scipy.misc.imresize(img_B, self.img_res)
-
+            
+            plt.imshow(img_A)
+            plt.show()
+            #plt.imshow(img_B)
+            #plt.show()
+            blocking = np.zeros(np.prod(img_A.shape))
+            blocking = np.reshape(blocking, img_A.shape)
+            xmin = np.random.randint(0,img_A.shape[0])
+            xmax = np.random.randint(xmin,img_A.shape[0])
+            ymin = np.random.randint(0,img_A.shape[1])
+            ymax = np.random.randint(ymin,img_A.shape[1])
+            blocking[xmin:xmax,ymin:ymax,:] = 1
+    
             # If training => do random flip
             if not is_testing and np.random.random() < 0.5:
                 img_A = np.fliplr(img_A)
                 img_B = np.fliplr(img_B)
-
+            blocked_A  = np.multiply (img_A , blocking)
+            #plt.imshow(blocked_A)
             imgs_A.append(img_A)
+            #plt.show()
             imgs_B.append(img_B)
 
         imgs_A = np.array(imgs_A)/127.5 - 1.
         imgs_B = np.array(imgs_B)/127.5 - 1.
 
-        return imgs_A, imgs_B
+        return imgs_A, imgs_B, Blocked_A
 
     def load_batch(self, batch_size=1, is_testing=False, datapath = ''):
         path = list()
@@ -64,14 +79,25 @@ class DataLoader():
 
                 img_A = scipy.misc.imresize(img_A, self.img_res)
                 img_B = scipy.misc.imresize(img_B, self.img_res)
-
+                blocking = np.zeros(np.prod(img_A.shape))
+                blocking = np.reshape(blocking, img_A.shape)
+                xmin = np.random.randint(0,img_A.shape[0])
+                xmax = np.random.randint(xmin,img_A.shape[0])
+                ymin = np.random.randint(0,img_A.shape[1])
+                ymax = np.random.randint(ymin,img_A.shape[1])
+                blocking[xmin:xmax,ymin:ymax,:] = 1
                 if not is_testing and np.random.random() > 0.5:
                         img_A = np.fliplr(img_A)
                         img_B = np.fliplr(img_B)
 
                 imgs_A.append(img_A)
                 imgs_B.append(img_B)
-
+                blocked_A  = np.multiply (img_A , blocking)
+                
+                plt.imshow(img_A)
+                plt.show()
+                plt.imshow(blocked_A)
+                plt.show()
             imgs_A = np.array(imgs_A)/127.5 - 1.
             imgs_B = np.array(imgs_B)/127.5 - 1.
 
